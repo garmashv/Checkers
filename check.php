fedca96c002ff4460a0b873a178d34bcf862ff5f
sinergetik <sinergetik77@gmail.com>
2019-07-20 19:27:40 +0300

    Multiple capture is implemented.

---
+++ check.php
@@ -0,0 +1,21 @@
+<?php // Скрипт проверки
+
+// Соединямся с БД
+$link = mysqli_connect("localhost", "checkers", "Checkers-123456", "checkers");
+
+if (isset($_COOKIE['id']) and isset($_COOKIE['hash'])) {
+    $query = mysqli_query($link, "SELECT *,INET_NTOA(user_ip) AS user_ip FROM users WHERE user_id = '" .
+        intval($_COOKIE['id']) . "' LIMIT 1");
+    $userdata = mysqli_fetch_assoc($query);
+
+    if (($userdata['user_hash'] !== $_COOKIE['hash']) or ($userdata['user_id'] !== $_COOKIE['id'])) {
+        setcookie("id", "", time() - 3600 * 24 * 30 * 12, "/");
+        setcookie("hash", "", time() - 3600 * 24 * 30 * 12, "/");
+        print "Хм, что-то не получилось";
+    } else {
+        print "Привет, " . $userdata['user_login'] . ". Всё работает!";
+    }
+} else {
+    print "Включите куки";
+}
+
---
+++ login.php
@@ -0,0 +1,45 @@
+<?php // Страница авторизации
+
+function generateCode($length = 6) // Функция для генерации случайной строки
+{
+    $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHI JKLMNOPRQSTUVWXYZ0123456789";
+    $code = "";
+    $clen = strlen($chars) - 1;
+    while (strlen($code) < $length) {
+        $code .= $chars[mt_rand(0, $clen)];
+    }
+    return $code;
+}
+
+// Соединямся с БД
+$link = mysqli_connect("localhost", "checkers", "Checkers-123456", "checkers");
+
+if (isset($_POST['submit'])) { // Вытаскиваем из БД запись, у которой логин равняеться введенному
+    $query = mysqli_query($link, "SELECT user_id, user_password FROM users WHERE user_login ='" .
+        mysqli_real_escape_string($link, $_POST['login']) . "' LIMIT 1");
+    $data = mysqli_fetch_assoc($query);
+
+    if ($data['user_password'] === md5(md5($_POST['password']))) { // Сравниваем пароли
+        $hash = md5(generateCode(10)); // Генерируем случайное число и шифруем его
+
+        // Записываем в БД новый хеш авторизации и IP
+        mysqli_query($link, "UPDATE users SET user_hash ='" . $hash . "' " . " WHERE user_id ='" .
+            $data['user_id'] . "'");
+
+        setcookie("id", $data['user_id'], time() + 60 * 60 * 24 * 30); // Ставим куки
+        setcookie("hash", $hash, time() + 60 * 60 * 24 * 30, null, null, null,
+            true); // httponly !!!
+
+        header("Location: check.php"); // Переадресовываем браузер на страницу проверки нашего скрипта
+        exit();
+
+    } else {
+        print "Вы ввели неправильный логин/пароль";
+    }
+}
+?>
+<form method="POST">
+    Логин <input name="login" type="text" required><br>
+    Пароль <input name="password" type="password" required><br>
+    <input name="submit" type="submit" value="Войти">
+</form>
