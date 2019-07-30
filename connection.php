5a42106306f95a6b1f5735481f08a0d7ea1b1c90
sinergetik <sinergetik77@gmail.com>
2019-07-30 08:44:16 +0300

    The initial location of the checkers is read from the MySQL database file on the server (it works correctly).

---
+++ connection.php
@@ -0,0 +1,9 @@
+<?php
+
+error_reporting(-1);
+$host = 'localhost'; // адрес сервера
+$database = 'checkers'; // имя базы данных
+$user = 'checkers'; // имя пользователя
+$password = 'Checkers-123456'; // пароль
+
+
