087502737f8a03d7572a95434cc1d7df86b0246d
sinergetik <sinergetik77@gmail.com>
2019-07-30 09:48:08 +0300

    The initial location of the checkers is read from the MySQL database file on the server (refactor).

--- getCheckersString.php
+++ getCheckersString.php
@@ -4,12 +4,12 @@ require_once 'connection.php';
 $link = mysqli_connect($host, $user, $password, $database)
 or die("Ошибка " . mysqli_error($link));
 
-$query ="SELECT * FROM board WHERE 1";
+$query ="SELECT board FROM board WHERE 1";
 $result = mysqli_query($link, $query) or die("Ошибка " . mysqli_error($link));
 $row = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
 if($result) {
-    echo substr(implode($row[0]), 1, 64); // строка шашек из массива выборки из БД
+    echo implode($row[0]); // строка шашек из массива выборки из БД
 }
 
 mysqli_close($link); // закрываем подключение к БД
