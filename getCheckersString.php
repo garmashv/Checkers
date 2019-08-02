5a64b8fec31d4f3cbdca54f291202e8c94934ae7
sinergetik <sinergetik77@gmail.com>
2019-08-02 10:45:21 +0300

    The string of checkers location is read from and write to the MySQL database on the server.

--- getCheckersString.php
+++ getCheckersString.php
@@ -1,15 +1,14 @@
 <?php
-
 require_once 'connection.php';
-$link = mysqli_connect($host, $user, $password, $database)
-or die("Ошибка " . mysqli_error($link));
 
-$query ="SELECT board FROM board WHERE 1";
-$result = mysqli_query($link, $query) or die("Ошибка " . mysqli_error($link));
-$row = mysqli_fetch_all($result, MYSQLI_ASSOC);
+$link = mysqli_connect($host, $user, $password, $database) or die("Ошибка " . mysqli_error($link));
+$query ="SELECT board FROM board WHERE 1"; // выбрать записи из табл. board, только поле board
+$result = mysqli_query($link, $query) or die("Ошибка " . mysqli_error($link)); // выполнить запрос
+$row = mysqli_fetch_all($result, MYSQLI_ASSOC); // строки результата - в массив
 
 if($result) {
-    echo implode($row[0]); // строка шашек из массива выборки из БД
+    echo implode($row[0]); // строка шашек из массива выборки из БД (0-я запись в таблице board)
 }
 
 mysqli_close($link); // закрываем подключение к БД
+?>
--- putCheckersString.php
+++ putCheckersString.php
@@ -1,12 +1,11 @@
 <?php
-
 require_once 'connection.php';
 
-$checkersString = $_POST['checkersString'];
+$checkersString = $_POST['checkersString']; // параметр, переданный данному скрипту в массиве POST (строка шашек)
 
 $link = mysqli_connect($host, $user, $password, $database) or die("Ошибка " . mysqli_error($link));
-$query = "UPDATE board SET board = '$checkersString'";
-$result = mysqli_query($link, $query) or die("Ошибка " . mysqli_error($link));
+$query = "UPDATE board SET board = '$checkersString'"; // записать строку с располож. шашек в БД MySQL
+$result = mysqli_query($link, $query) or die("Ошибка " . mysqli_error($link)); // выполнить запрос
 mysqli_close($link); // закрываем подключение к БД
 
 ?>
